f48b71f12e2afe4e633dcf8f1eb2063d6744a4de
wing <wau1@uw.edu>
2025-04-26 16:37:50 -0700

    Create html code for ai model template component
    Todo: create the tailwind css code for this model

--- app/catalog/chat-gpt/page.tsx
+++ app/catalog/chat-gpt/page.tsx
@@ -1,6 +1,25 @@
 //DO NOT WRITE CODE FROM SCRATCH, USE TEMPLATE FOR ALL MODELS CREATE TEMPLATE WITHIN THIS MODEL'S PAGE
 export default function Home() {
     return (
-        <p>hi!</p>
-    )
-};
+        <div className="flex">
+            <img src="/home/wing/repos/pandoras_box/public/file.svg" alt="sdfsdf" />
+            <h1 className="text-red-200">AI Model Name</h1>
+            <a href="https://openai.com/index/chatgpt/">model name</a>
+            <a href="https://platform.openai.com/docs/guides/text">model name docs</a>
+            <h1>Introduction</h1>
+            <p>Information about the model</p>
+            <ol>Instructions
+                <li> step 1</li>
+                <li> step 2</li>
+                <li> step 3</li>
+                <li> step 4</li>
+                <li> step 5</li>
+                <li> step 6</li>
+            </ol>
+            <div className="absolute left-1/2 -ml-0.5 w-0.5 h-screen bg-gray-600"></div>
+            <h2 className="absolute ">Table of Contents</h2>
+        </div>
+    );
+}
+
+
