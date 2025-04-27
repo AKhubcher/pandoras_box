c8083e033c07e9d6934fe1c01c25eda623172aab
Lakshmi Pachipulusu <lakshmi@metrohub.io>
2025-04-26 17:41:58 -0700

    finished categories, now working on analysis

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <main>
-            <h1 style={{ textAlign: 'center', marginTop: '40px', fontSize: 70, color: '#5A9BD4', fontWeight: 50 }}>All Models</h1>
+            <h1 style={{ textAlign: 'center', marginTop: '20px', fontSize: 70, color: '#5A9BD4', fontWeight: 50 }}>All Models</h1>
 
             {/* Cards */}
             <div style={{
@@ -17,21 +17,22 @@ export default function Home() {
                     color: 'white',
                     borderRadius: '15px',
                     padding: '20px',
-                    width: '250px',
+                    width: '350px',
                     textAlign: 'center',
                     marginRight: 100
                 }}>
-                    <h2>Analysis</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <h2 style={{ fontSize: 30, marginBottom: 10 }}>Analysis</h2>
+                    <p style={{ fontSize: 18 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                     <button style={{
                         backgroundColor: '#FFF4B8',
                         border: 'none',
                         padding: '10px',
                         marginTop: '20px',
                         borderRadius: '10px',
+                        width: 200,
                         cursor: 'pointer',
-                        color: 'black'
-                    }}>Learn More</button>
+                        color: 'black',
+                    }}><a href="categories/analysis">Learn More</a></button>
                 </div>
 
                 {/* Generative Card */}
@@ -40,17 +41,18 @@ export default function Home() {
                     color: 'white',
                     borderRadius: '15px',
                     padding: '20px',
-                    width: '250px',
-                    textAlign: 'center'
+                    width: '350px',
+                    textAlign: 'center',
                 }}>
-                    <h2>Generative</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <h2 style={{ fontSize: 30, marginBottom: 10 }}>Generative</h2>
+                    <p style={{ fontSize: 18 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                     <button style={{
                         backgroundColor: '#FFF4B8',
                         border: 'none',
                         padding: '10px',
                         marginTop: '20px',
                         borderRadius: '10px',
+                        width: 200,
                         cursor: 'pointer',
                         color: 'black'
                     }}>Learn More</button>
@@ -58,7 +60,7 @@ export default function Home() {
             </div>
 
             {/* Coming Soon */}
-            <h2 style={{ textAlign: 'center', marginBottom: '40px', marginTop: '50px' }}>More Coming Soon...</h2>
+            <h2 style={{ textAlign: 'center', marginBottom: '40px', marginTop: '50px', fontSize: 50 }}>More Coming Soon...</h2>
         </main>
     );
 }
