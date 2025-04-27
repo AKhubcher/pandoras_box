742b5cca5f05d900050ee2789c9e7a406d345be1
jhaligowska <julhal@uw.edu>
2025-04-26 20:27:04 -0700

    learn page

---
+++ app/learn/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Learn() {
+  return (
+    <div className="min-h-screen py-20 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-4xl sm:text-9xl font-bold mb-12 text-center bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent">
+          Learn
+        </h1>
+
+        <p className="text-xl text-gray-300 text-center leading-relaxed">
+          Coming soon!
+        </p>
+        
+      </div>
+    </div>
+  );
+} 
